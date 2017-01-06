14e4936d93bed1a94615941677bc3bee2ec2daca
Marcus Bitzl <marcus@bitzl.com>
2017-01-06 20:40:25 +0100

    Fixed computation.

--- install/cli.sh
+++ install/cli.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ "$[$(date +%s) - $(stat -c %Z /var/lib/apt/periodic/update-success-stamp)]" -ge 600000 ]; then
-apt-get update
+  apt-get update
 fi
 
-apt-get -y install most tmux
+sudo apt-get -y install most tmux zsh
