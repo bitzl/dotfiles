0804973ef9c79b7d691567d9ea406bb7f028bb33
Marcus Bitzl <marcus@bitzl.com>
2017-01-06 20:37:23 +0100

    Extended setup.

--- setup_prezto.zsh
+++ setup_prezto.zsh
@@ -1,7 +1,13 @@
 #!/bin/zsh
 
+if [ ! -d "${ZDOTDIR:-$HOME}/.zprezto" ]; then
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+fi
+
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
+rm "${ZDOTDIR:-$HOME}/.zpreztorc"
+ln -s "dotfiles/prezto/zpreztorc" "${ZDOTDIR:-$HOME}/.zpreztorc"
